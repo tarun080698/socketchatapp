f56d10e54a36762c674014046dba09914f187190
tarun080698 <tarun3dadlani@gmail.com>
2021-01-05 19:43:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import {connect} from 'react-redux'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path='/login'
+            render={props => {
+              return <h1>Login</h1>
+            }}
+          />
+          <Route
+            path='/'
+            render={props => {
+              return <h1>Root</h1>
+            }}
+          />
+        </Switch>  
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state.auth
+})
+
+const mapDispatchToProps = dispatch => ({
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './store/reducers'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const {store, persistor} = configureStore()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
