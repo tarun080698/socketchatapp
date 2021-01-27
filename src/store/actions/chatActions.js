8b595c3abc674e0807c2649e64bfdec02e663e3b
tarun080698 <tarun3dadlani@gmail.com>
2021-01-27 09:01:39 +0530

    fixed signup call

--- src/store/actions/chatActions.js
+++ src/store/actions/chatActions.js
@@ -1,6 +1,6 @@
 export const setupSocket = () => {
   return (dispatch) => {
-    const socket = new WebSocket("ws://localhost:8080");
+    const socket = new WebSocket("ws://localhost:8081");
     socket.onopen = () => {
       dispatch({
         type: "SETUP",
